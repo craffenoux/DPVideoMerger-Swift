e91ba3ab4e3243ea8377efa588902a1084e3447a
Clément Raffenoux <clement.raffenoux@gmail.com>
2021-10-07 19:20:20 +0200

    add merge videos with music

--- DPVideoMerger/DPVideoMerger/DPVideoMerger.swift
+++ DPVideoMerger/DPVideoMerger/DPVideoMerger.swift
@@ -13,6 +13,7 @@ import AVKit
 
 @objc protocol VideoMerger {
     func mergeVideos(withFileURLs videoFileURLs: [URL], videoResolution:CGSize, videoQuality:String, completion: @escaping (_ mergedVideoURL: URL?, _ error: Error?) -> Void)
+    func mergeVideos(withFileURLs videoFileURLs: [URL], videoResolution:CGSize, videoQuality:String, audioUrl: URL, isRepeatingAudio: Bool, completion: @escaping (_ mergedVideoURL: URL?, _ error: Error?) -> Void)
     func gridMergeVideos(withFileURLs videoFileURLs: [URL], matrix: DPVideoMatrix, audioFileURL: URL?, videoResolution: CGSize, isRepeatVideo: Bool, isRepeatAudio: Bool, isAudio: Bool,videoDuration: Int, videoQuality: String, completion: @escaping (_ mergedVideoURL: URL?, _ error: Error?) -> Void)
     func parallelMergeVideos(withFileURLs videoFileURLs: [URL], audioFileURL: URL?, videoResolution: CGSize, isRepeatVideo: Bool, isRepeatAudio: Bool, videoDuration: Int, videoQuality: String, alignment: ParallelMergeAlignment, completion: @escaping (_ mergedVideoURL: URL?, _ error: Error?) -> Void)
 }
@@ -41,6 +42,172 @@ import AVKit
 }
 // MARK:-  Public Functions
 extension DPVideoMerger : VideoMerger {
+    func mergeVideos(withFileURLs videoFileURLs: [URL], videoResolution: CGSize, videoQuality: String, audioUrl: URL, isRepeatingAudio: Bool, completion: @escaping (URL?, Error?) -> Void) {
+        if videoFileURLs.count <= 1 {
+            DispatchQueue.main.async { completion(nil, self.videoMoreThenOneError()) }
+            return
+        }
+        let composition = AVMutableComposition()
+        guard let videoTrack: AVMutableCompositionTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+            DispatchQueue.main.async { completion(nil, self.videoTarckError()) }
+            return
+        }
+        var instructions = [AVVideoCompositionInstructionProtocol]()
+        var isError = false
+        var currentTime: CMTime = CMTime.zero
+        var videoSize = CGSize.zero
+        var highestFrameRate = 0
+        if videoResolution == CGSize(width: -1, height: -1) {
+        for  videoFileURL in videoFileURLs {
+            let options = [AVURLAssetPreferPreciseDurationAndTimingKey: true]
+            let asset = AVURLAsset(url: videoFileURL, options: options)
+            guard let videoAsset: AVAssetTrack = asset.tracks(withMediaType: .video).first else {
+                return
+            }
+            if videoSize.equalTo(CGSize.zero) {
+                videoSize = (videoAsset.naturalSize)
+            }
+            var isVideoAssetPortrait_ = false
+            let videoTransform: CGAffineTransform = videoAsset.preferredTransform
+            
+            if videoTransform.a == 0 && videoTransform.b == 1.0 && videoTransform.c == -1.0 && videoTransform.d == 0 {
+                isVideoAssetPortrait_ = true
+            }
+            if videoTransform.a == 0 && videoTransform.b == -1.0 && videoTransform.c == 1.0 && videoTransform.d == 0 {
+                isVideoAssetPortrait_ = true
+            }
+            
+            var videoAssetWidth: CGFloat = videoAsset.naturalSize.width
+            var videoAssetHeight: CGFloat = videoAsset.naturalSize.height
+            if isVideoAssetPortrait_ {
+                videoAssetWidth = videoAsset.naturalSize.height
+                videoAssetHeight = videoAsset.naturalSize.width
+            }
+            
+            if videoSize.height < (videoAssetHeight) {
+                videoSize.height = (videoAssetHeight)
+            }
+            if videoSize.width < (videoAssetWidth) {
+                videoSize.width = (videoAssetWidth)
+            }
+        }
+        } else {
+            if (videoResolution.height < 100 || videoResolution.width < 100) {
+                DispatchQueue.main.async { completion(nil, self.videoSizeError()) }
+                return
+            }
+            videoSize = videoResolution
+        }
+        
+        for  videoFileURL in videoFileURLs {
+            let options = [AVURLAssetPreferPreciseDurationAndTimingKey: true]
+            let asset = AVURLAsset(url: videoFileURL, options: options)
+            guard let videoAsset: AVAssetTrack = asset.tracks(withMediaType: .video).first else {
+                DispatchQueue.main.async { completion(nil, self.videoTarckError()) }
+                return
+            }
+            let allAudioAssets = asset.tracks(withMediaType: .audio)
+            guard let _: AVAssetTrack = asset.tracks(withMediaType: .audio).first else {
+                DispatchQueue.main.async {completion(nil, self.audioTarckError()) }
+                return
+            }
+            let currentFrameRate = Int(roundf((videoAsset.nominalFrameRate)))
+            highestFrameRate = (currentFrameRate > highestFrameRate) ? currentFrameRate : highestFrameRate
+            let trimmingTime: CMTime = CMTimeMake(value: Int64(lround(Double((videoAsset.nominalFrameRate) / (videoAsset.nominalFrameRate)))), timescale: Int32((videoAsset.nominalFrameRate)))
+            let timeRange: CMTimeRange = CMTimeRangeMake(start: trimmingTime, duration: CMTimeSubtract((videoAsset.timeRange.duration), trimmingTime))
+            do {
+                try videoTrack.insertTimeRange(timeRange, of: videoAsset, at: currentTime)
+                for audioAsset in allAudioAssets {
+                    guard let audioTrack: AVMutableCompositionTrack = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+                        DispatchQueue.main.async { completion(nil, self.audioTarckError()) }
+                        return
+                    }
+                    try audioTrack.insertTimeRange(timeRange, of: audioAsset, at: currentTime)
+                }
+                
+                let videoCompositionInstruction = AVMutableVideoCompositionInstruction.init()
+                videoCompositionInstruction.timeRange = CMTimeRangeMake(start: currentTime, duration: timeRange.duration)
+                let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack)
+                
+                var isVideoAssetPortrait_ = false
+                let videoAssetOrientation_ = videoAssetOrientation(videoAsset)
+                if videoAssetOrientation_ == .right || videoAssetOrientation_ == .left { isVideoAssetPortrait_ = true }
+                
+                var videoAssetWidth: CGFloat = videoAsset.naturalSize.width
+                var videoAssetHeight: CGFloat = videoAsset.naturalSize.height
+                if isVideoAssetPortrait_ {
+                    videoAssetWidth = videoAsset.naturalSize.height
+                    videoAssetHeight = videoAsset.naturalSize.width
+                }
+
+                
+                
+                var tx: Int = 0
+                if videoSize.width - videoAssetWidth != 0 {
+                    tx = Int((videoSize.width - videoAssetWidth) / 2)
+                }
+                var ty: Int = 0
+                if videoSize.height - videoAssetHeight != 0 {
+                    ty = Int((videoSize.height - videoAssetHeight) / 2)
+                }
+                var Scale = CGAffineTransform(scaleX: 1, y: 1)
+                var factor : CGFloat = 1.0
+                if tx != 0 && ty != 0 {
+                    if tx <= ty {
+                        factor = CGFloat(videoSize.width / videoAssetWidth)
+                        Scale = CGAffineTransform(scaleX: CGFloat(factor), y: CGFloat(factor))
+                        tx = 0
+                        ty = Int((videoSize.height - videoAssetHeight * CGFloat(factor)) / 2)
+                    }
+                    if tx > ty {
+                        factor = CGFloat(videoSize.height / videoAssetHeight)
+                        Scale = CGAffineTransform(scaleX: CGFloat(factor), y: CGFloat(factor))
+                        ty = 0
+                        tx = Int((videoSize.width - videoAssetWidth * CGFloat(factor)) / 2)
+                    }
+                }
+               
+                
+                var Move: CGAffineTransform!
+                var transform: CGAffineTransform!
+                switch videoAssetOrientation_ {
+                case UIImage.Orientation.right:
+                    Move = CGAffineTransform(translationX: (videoAssetWidth * factor) + CGFloat(tx)  , y: CGFloat(ty))
+                    transform = CGAffineTransform(rotationAngle: degreeToRadian(90))
+                    layerInstruction.setTransform(transform.concatenating(Scale.concatenating(Move)), at: .zero)
+                case UIImage.Orientation.left:
+                    Move = CGAffineTransform(translationX: CGFloat(tx), y: videoSize.height - CGFloat(ty))
+                    transform = CGAffineTransform(rotationAngle: degreeToRadian(270))
+                    layerInstruction.setTransform(transform.concatenating(Scale.concatenating(Move)), at: .zero)
+                case UIImage.Orientation.up:
+                    Move = CGAffineTransform(translationX: CGFloat(tx), y: CGFloat(ty))
+                    layerInstruction.setTransform(Scale.concatenating(Move), at: .zero)
+                case UIImage.Orientation.down:
+                    Move = CGAffineTransform(translationX: videoSize.width + CGFloat(tx), y: (videoAssetHeight*factor)+CGFloat(ty))
+                    transform = CGAffineTransform(rotationAngle: degreeToRadian(180))
+                    layerInstruction.setTransform(transform.concatenating(Scale.concatenating(Move)), at: .zero)
+                default:
+                    break;
+                }
+//                let Move = CGAffineTransform(translationX: CGFloat(tx), y: CGFloat(ty))
+//                layerInstruction.setTransform(Scale.concatenating(Move), at: CMTime.zero)
+                videoCompositionInstruction.layerInstructions = [layerInstruction]
+                instructions.append(videoCompositionInstruction)
+                currentTime = CMTimeAdd(currentTime, timeRange.duration)
+            } catch {
+                debugPrint("Unable to load data: \(error)")
+                isError = true
+                DispatchQueue.main.async { completion(nil, error) }
+            }
+        }
+        
+        addAudioToMergedVideo(audioUrl, composition, true, currentTime, completion)
+        
+        if isError == false {
+            exportMergedVideo(instructions, highestFrameRate, videoSize, composition, videoQuality, completion)
+        }
+    }
+    
     /// Multiple videos merge in one video with manage scale & aspect ratio
     /// - Parameters:
     ///   - videoFileURLs: Video file path URLs, Array of videos that going to merge
